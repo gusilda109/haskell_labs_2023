6ce4757d68f88ad5eb66686f8892f52111bfa001
gusilda109 <34038780+gusilda109@users.noreply.github.com>
2023-09-20 01:00:13 +0700

    LABA NOMER NOL'

---
+++ main.hs
@@ -0,0 +1,13 @@
+-- Write a function that accepts three arguments (a,b,c) and returns a solution to the quadratic equasion ax^2 + bx + c = 0
+-- The return type should be a pair of floating-point numbers. In case there is no solution, function should throw error (see example below) 
+-- Also, write the type of solveQuadratic function
+-- solveQuadratic ::
+solveQuadratic :: Float -> Float -> Float -> (Float, Float)
+
+solveQuadratic a b c = if (b ^ 2 - 4 * a * c < 0) then error "ERROR!!!!!!ASHIBKA" 
+else ((-(b) - sqrt(b ^ 2 - 4 * a * c))/ (2 * a), (-(b) + sqrt(b ^ 2 - 4 * a * c))/(2 * a))
+
+main :: IO()
+main = do
+    let result = solveQuadratic (-1) 1 (-8)
+    print result
