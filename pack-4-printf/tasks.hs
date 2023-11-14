f531c27d1881624d90ccf2337a0d5cf0d3353c54
gusilda109 <m.erchenko@g.nsu.ru>
2023-11-14 15:04:00 +0700

    Update tasks.hs

---
+++ pack-4-printf/tasks.hs
@@ -0,0 +1,36 @@
+import Data.Char(digitToInt)
+
+decToBin' :: Int -> [Int]
+
+decToBin' n
+    | n == 1 = [1]
+    | n == 0 = [0]
+    | otherwise = decToBin'(n `div` 2)  ++ [n `mod` 2]
+
+baseNToDecimal :: Integer -> [Integer] -> Integer
+baseNToDecimal base digits = foldl (\acc x -> acc * base + x) 0 digits
+
+strToInt :: String -> Int
+strToInt str = foldl (\acc x -> acc * 10 + digitToInt x) 0 str
+
+findMissing :: [Int] -> Int
+findMissing xs = findMissing' 1 xs
+  where
+    findMissing' n [] = n
+    findMissing' n (x:xs)
+      | n == x = findMissing' (n + 1) xs
+      | otherwise = n
+
+parenthesesChecker :: String -> Bool
+parenthesesChecker s = checker s 0
+
+checker :: String -> Int -> Bool
+checker [] 0 = True
+checker [] _ = False
+checker ('(':xs) count = checker xs (count + 1)
+checker (')':xs) count
+  | count > 0  = checker xs (count - 1)
+  | otherwise = False
+checker (_:xs) count = checker xs count
+
+
