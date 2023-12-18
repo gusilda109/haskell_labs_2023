b56c86bb89082786ab100cad5311d26a96846d2d
gusilda109 <m.erchenko@g.nsu.ru>
2023-12-18 22:03:19 +0700

    Update complex.hs

--- pack-5-data-structures/complex.hs
+++ pack-5-data-structures/complex.hs
@@ -13,6 +13,10 @@ multiply :: Number -> Number -> Number
 multiply (Number a1 b1) (Number a2 b2) =
     Number (a1 * a2 - b1 * b2) (a1 * b2 + b1 * a2)
 
+divide :: Number -> Number -> Number
+divide (Number r1 i1) (Number r2 i2) =
+    Number ((r1 * r2 + i1 * i2) / denominator) ((i1 * r2 - r1 * i2) / denominator)
+    where denominator = r2^2 + i2^2
 
 conjugate :: Number -> Number
 conjugate (Number a b) = Number a (-b)
